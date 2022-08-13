4e96dad4089286ecfef2c46412d77b504c01608e
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-12 20:37:52 -0500

    fixed bug

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template lang="">
   <div class="text-center">
     <div v-if="$colorMode.preference === 'dark'" class="dark-home-bg"></div>
-    <div v-else class="light-home-bg"></div>
+    <div v-if="$colorMode.preference === 'light'" class="light-home-bg"></div>
     <NuxtPage />
   </div>
 </template>
@@ -9,22 +9,6 @@
 <script setup></script>
 
 <style lang="css">
-.dark-home-bg {
-  width: 50%;
-  margin: 0 auto;
-  height: 100vh;
-  background-image: linear-gradient(
-      90deg,
-      #00010d 0%,
-      #00010d 10%,
-      transparent 50%
-    ),
-    url("@/images/darkMode-cricadev.png");
-  background-size: auto 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  transform: translateX(5%);
-}
 .light-home-bg {
   width: 50%;
   margin: 0 auto;
@@ -36,6 +20,22 @@
       transparent 50%
     ),
     url("@/images/lightMode-cricadev.png");
+  background-size: auto 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  transform: translateX(5%);
+}
+.dark-home-bg {
+  width: 50%;
+  margin: 0 auto;
+  height: 100vh;
+  background-image: linear-gradient(
+      90deg,
+      #00010d 0%,
+      #00010d 10%,
+      transparent 50%
+    ),
+    url("@/images/darkMode-cricadev.png");
   background-size: auto 100%;
   background-position: center;
   background-repeat: no-repeat;
