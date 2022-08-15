07468fd2959c473a1f60615f1b63248c6d3c5852
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-15 17:56:09 -0500

    new routes for images

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -17,14 +17,14 @@ import { ProjectComp } from '../.nuxt/components';
     <div class="flex flex-col overflow-y-auto py-96">
       <ProjectComp
         title="FinanceGod"
-        src="../images/finacegod-bg.png"
+        src="../assets/financegod.png"
       ></ProjectComp>
-      <ProjectComp title="MathGod" src="../images/mathgod.png"></ProjectComp>
+      <ProjectComp title="MathGod" src="../assets/mathgod.png"></ProjectComp>
       <ProjectComp
         title="CricaDev"
-        src="../images/cricadev-project.png"
+        src="../assets/cricadev-project.png"
       ></ProjectComp>
-      <ProjectComp title="TEF" src="../images/tef.png"></ProjectComp>
+      <ProjectComp title="TEF" src="../assets/tef.png"></ProjectComp>
     </div>
     <NuxtPage />
   </div>
