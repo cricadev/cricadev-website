8358c86c601a444e5f9531985720e659217b8073
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-16 11:05:37 -0500

    updated path for icon

--- app.vue
+++ app.vue
@@ -24,7 +24,7 @@ useHead({
         "Hey, I'm Cristian Casallas also known as CricaDev, A FullStack Web Developer from Colombia with a variety of qualifications and experience in the industry. Curious Risk-taking Innovative Creative Ambitious Determined Enthusiastic Valuable.",
     },
   ],
-  link: [{ rel: "icon", type: "image/x-icon", href: "./public/cricadev.png" }],
+  link: [{ rel: "icon", type: "image/png", href: "/cricadev.png" }],
 });
 </script>
 <style></style>
