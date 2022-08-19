3a9ff1a878bc24f1773a37f8f46592f1990ed2b4
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-18 20:55:11 -0500

    rebuilding with grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div
-    class="grid w-screen h-screen min-h-screen grid-cols-12 grid-rows-6 overflow-hidden"
+    class="grid w-screen h-screen min-h-screen grid-cols-12 grid-rows-6 home"
   >
     <div v-if="$colorMode.value === 'dark'" class="dark-home-bg home-bg"></div>
     <div
@@ -12,19 +12,19 @@
       technology. <br />
       <span class="italic font-normal">I never stop learning.</span>
     </p>
+    <div class="line-container">
+      <div class="line-1 line"></div>
+      <div class="line-2 line"></div>
+    </div>
     <div class="home-figure">
-      <div class="colombian-section">
-        <div
-          class="border-[1px] border-b-0 border-l-0 border-t-white-1 border-r-white-1 square"
-        ></div>
-      </div>
       <p class="font-thin text-center text-base_d">
         I am a colombian<br />
         <span class="italic font-normal">Web Developer</span>
       </p>
     </div>
+
     <ul
-      class="absolute flex flex-col right-28 top-1/2 translate-y-[-50%] text-left text-xl_d italic font-extralight space-y-4"
+      class="flex flex-col gap-2 italic text-left text-xl_d font-extralight home-list"
     >
       <li class="item">Risk-taking</li>
       <li class="item">Innovative</li>
@@ -111,7 +111,10 @@
 .home-bg {
   grid-column: 1 / 13;
   grid-row: 1 / 7;
-  background-size: contain;
+  width: 100%;
+  height: 100%;
+  transform: translateX(3%);
+  background-size: auto 100%;
   background-position: center;
   background-repeat: no-repeat;
 }
@@ -119,8 +122,9 @@
   background-image: linear-gradient(
       90deg,
       #f6f6f6 0%,
-      #f6f6f6 10%,
-      transparent 100%
+      #f6f6f6 20%,
+      #f6f6f6 30%,
+      transparent 40%
     ),
     url("@/images/lightMode-cricadev.png");
 }
@@ -128,15 +132,72 @@
   background-image: linear-gradient(
       90deg,
       #00010d 0%,
-      #00010d 10%,
-      transparent 100%
+      #00010d 20%,
+      #00010d 30%,
+
+      transparent 40%
     ),
     url("@/images/darkMode-cricadev.png");
 }
+.home {
+  place-items: center;
+}
 .home-paragraph {
   grid-column: 2 / 5;
-  grid-row: 3 / 4;
+  grid-row: 3 / 5;
+  z-index: 2;
+
   transition: 1s ease-in-out all;
   animation: home-paragraph-animation 1s ease-in-out forwards;
+}
+.home-figure {
+  grid-column: 8 / 10;
+  grid-row: 4 / 5;
+  z-index: 2;
+  place-self: end center;
+  transition: 1s ease-in-out all;
+  animation: home-figure-animation 1s ease-in-out forwards;
+}
+.home-list {
+  grid-column: 11 / 12;
+  grid-row: 3 / 5;
+  transition: 1s ease-in-out all;
+  animation: home-list-animation 1s ease-in-out forwards;
+}
+.line-container {
+  grid-column: 8 / 10;
+  grid-row: 4 / 5;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+}
+.line {
+  background: #f6f6f6;
+
+  animation: grow 1s linear forwards;
+}
+.line-1 {
+  width: 100%;
+  height: 1px;
+  grid-column: 1 / 3;
+  place-self: end;
+}
+.line-2 {
+  width: 1px;
+  height: 100%;
+  grid-column: 3 / 4;
+  grid-row: 1 / 3;
+  place-self: end;
+}
+@keyframes grow {
+  0% {
+    width: 0;
+  }
+  100% {
+    width: 100%;
+  }
 }
 </style>
