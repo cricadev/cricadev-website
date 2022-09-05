3f8a56f0d42669abaed5ac4f0113d8e5a0139bbc
CricaDev <cristiancasallas998@gmail.com>
2022-09-04 21:05:07 -0500

    project updated

--- components/projectComp.vue
+++ components/projectComp.vue
@@ -6,14 +6,31 @@
     class="grid w-10/12 h-20 grid-cols-5 grid-rows-5 project-container"
   >
     <div class="bg"></div>
-    <h2 class="font-black text-center text-base_m no-hover-h2">{{ title }}</h2>
-    <h2 class="font-black text-center hover-h2 text-xs_m">{{ title }}</h2>
+    <h2 class="font-black text-center text-base_m no-hover-h2">
+      {{ title }}
+    </h2>
+    <h2 class="font-black text-center hover-h2 text-lg_m">{{ title }}</h2>
     <p class="hover-p">{{ content }}</p>
+    <div class="flex gap-4 box-buttons">
+      <a href="" class="button-1 btn"
+        >code <span><Icon icon="ant-design:code-filled" /> </span
+      ></a>
+      <a href="" class="button-2 btn"
+        >Project <span><Icon icon="carbon:view-filled" /> </span
+      ></a>
+    </div>
+
+    <div class="flex flex-col icons">
+      <Icon icon="cib:nuxt-js" />
+      <Icon icon="akar-icons:vue-fill" />
+      <Icon icon="bxl:tailwind-css" />
+    </div>
     <img :src="img == 'none' ? '../images/coming-soon.jpg' : img" alt="" />
   </transition-group>
   <!--</div>-->
 </template>
 <script setup>
+import { Icon } from "@iconify/vue";
 defineProps({
   title: String,
   img: String,
@@ -37,7 +54,19 @@ defineProps({
   cursor: pointer;
   place-items: center;
   border-radius: 10%;
-
+  .box-buttons {
+    grid-column: 1/6;
+    grid-row: 5/6;
+    opacity: 0;
+  }
+  .btn {
+    border-radius: 20px;
+  }
+  .icons {
+    opacity: 0;
+    grid-column: 5/6;
+    grid-row: 3/5;
+  }
   .hover-p {
     opacity: 0;
     grid-column: 1/6;
@@ -45,12 +74,13 @@ defineProps({
     width: 80%;
     font-size: 0.75rem;
     place-self: center start;
-    padding-left: 20px;
+    padding-left: 25px;
   }
   .hover-h2 {
     opacity: 0;
-    grid-column: 1/2;
+    grid-column: 1/3;
     grid-row: 3/4;
+    padding-left: 40px;
   }
 
   .bg {
@@ -70,8 +100,8 @@ defineProps({
     z-index: 1;
   }
   .no-hover-h2 {
-    grid-column: 2/4;
-    grid-row: 2/4;
+    grid-column: 3/4;
+    grid-row: 3/4;
     z-index: 3;
     color: #f6f6f6;
   }
@@ -99,6 +129,14 @@ defineProps({
     .hover-p {
       opacity: 1;
       transition: 0.5s ease-in-out;
+    }
+    .icons {
+      opacity: 1;
+      transition: 0.5s all ease-in-out;
+    }
+    .box-buttons {
+      opacity: 1;
+      transition: 0.5s all ease-in-out;
     }
   }
 }
