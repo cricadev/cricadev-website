0cacb63853e4a103baf2f7b86144b14adbf45391
CricaDev <cristiancasallas998@gmail.com>
2022-09-07 08:24:45 -0500

    pwa implemented

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,20 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "nuxt-icon",
+    "@kevinmarrec/nuxt-pwa",
+  ],
+  pwa: {
+    manifest: {
+      name: "CricaDev",
+      short_name: "CricaDev",
+      theme_color: "#f6f6f6",
+      lang: "en",
+    },
+  },
   tailwindcss: {
     cssPath: "@/assets/css/tailwind.css",
     configPath: "tailwind.config.js",
