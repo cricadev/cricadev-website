37cbc2942b23b8fc5ed48dc5777371dab0028774
CricaDev <cristiancasallas998@gmail.com>
2022-09-13 21:05:19 -0500

    about me

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,20 @@
 <template lang="">
-  <div class="pt-16">
-    <p class="font-black text-xl4_m">
-      <span>C</span>
-      <span>R</span>
-      <span>I</span>
-      <span>C</span>
-      <span>A</span>
-      <span>D</span>
-      <span>E</span>
-      <span>V</span>
-    </p>
+  <div class="">
+    <div
+      class="pt-20 pb-8 ml-6 font-black text-left text-xl3_m xss-m:text-xl4_m about-container"
+    >
+      <span class="letter c1">C </span><span class="about-link">urious</span>
+      <span class="letter r">R </span
+      ><span class="about-link">isk-taking</span>
+      <span class="letter i">I </span><span class="about-link">nnovative</span>
+      <span class="letter c2">C </span><span class="about-link">reative</span>
+      <span class="letter a">A </span><span class="about-link">mbitious</span>
+      <span class="letter d">D </span><span class="about-link">etermined</span>
+      <span class="letter e">E </span
+      ><span class="about-link">nthusistic</span>
+      <span class="letter v">V </span><span class="about-link">aluable</span>
+    </div>
+
     <NuxtPage />
   </div>
 </template>
@@ -19,15 +24,27 @@ useHead({
 });
 </script>
 <style lang="scss" scoped>
-p {
-  display: flex;
-  flex-direction: column;
+.about-container {
+  display: grid;
+  grid-template-rows: repeat(8, minmax(10px, 1fr));
+  grid-template-columns: 15% 85%;
+  place-content: start;
+  place-items: center;
   height: 100vh;
   width: 100%;
+  @media (min-width: 386px) {
+    grid-template-columns: 20% 80%;
+  }
+  .letter {
+    cursor: pointer;
+    grid-column: 1/2;
+    place-self: center;
+  }
 
-  span {
-    width: min-content;
-    height: min-content;
+  .about-link {
+    padding-left: 20px;
+    grid-column: 2/3;
+    place-self: center start;
   }
 }
 </style>
