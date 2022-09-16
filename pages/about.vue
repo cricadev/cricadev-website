4d719f05450ce453fb1f406f06d0d9b404be0a2a
CricaDev <cristiancasallas998@gmail.com>
2022-09-16 09:28:53 -0500

    no selectable text

--- pages/about.vue
+++ pages/about.vue
@@ -471,6 +471,13 @@ onBeforeMount(async () => {
     grid-column: 1/2;
     place-self: center;
     transition: 0.4s all ease-in-out;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
     /*
     background-clip: text;
     -webkit-background-clip: text;
