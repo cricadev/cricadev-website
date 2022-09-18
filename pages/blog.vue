81ed32a463cf1bbba53ffec3818539c8d3cdd4c8
CricaDev <cristiancasallas998@gmail.com>
2022-09-18 11:11:32 -0500

    slider cfreated mobile

--- pages/blog.vue
+++ pages/blog.vue
@@ -10,15 +10,23 @@ import { BlogComp } from '../.nuxt/components';
       Let's talk about technology
     </p>
     <p class="pt-8 pb-4 font-medium text-base_t">Recent</p>
-    <BlogComp
-      title="Self-education: how it save my life"
-      content="Just like you, I didn't really fit anywhere when I was a kid. I still don’t, so I've built my own place. Modern psychology has evolved to understand a..."
-      img="../blog-01.jpg"
-      Avatar="../avatar-crica.jpg"
-      Author="Cristian Casallas"
-      date="18 Sep, 2022"
-      Duration="18"
-    ></BlogComp>
+    <carousel :items-to-show="1">
+      <slide v-for="slide in 3" :key="slide">
+        <BlogComp
+          title="Self-education: how it save my life"
+          content="Just like you, I didn't really fit anywhere when I was a kid. I still don’t, so I've built my own place. Modern psychology has evolved to understand a..."
+          img="../blog-01.jpg"
+          Avatar="../avatar-crica.jpg"
+          Author="Cristian Casallas"
+          date="18 Sep, 2022"
+          Duration="18"
+        ></BlogComp>
+      </slide>
+
+      <template #addons>
+        <navigation />
+      </template>
+    </carousel>
     <div class="pb-4 blogs-order">
       <p class="pt-4 font-medium text-sm_m">All Blogs</p>
       <BlogComp
@@ -49,15 +57,41 @@ import { BlogComp } from '../.nuxt/components';
         Duration="18"
       ></BlogComp>
     </div>
+
     <NuxtPage />
   </div>
 </template>
-<script setup>
-useHead({
-  title: "CricaDev | Blog",
-});
+<script>
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+
+export default {
+  name: "App",
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+};
 </script>
 <style lang="scss">
+.dark .carousel__prev,
+.dark .carousel__next {
+  background: #f6f6f6;
+  .carousel__icon {
+    fill: #00010d;
+  }
+}
+.carousel__prev,
+.carousel__next {
+  background: #000;
+}
+.carousel__prev--in-active,
+.carousel__next--in-active {
+  display: none;
+}
+
 .blogs-order {
   display: flex;
   flex-direction: column;
