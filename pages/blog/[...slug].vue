43640802458297729d582ef309ae722609477962
CricaDev <cristiancasallas998@gmail.com>
2022-09-20 16:35:11 -0500

    gradient blog updated

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -6,6 +6,9 @@ const { data: blogPost } = await useAsyncData(`content-${path}`, () => {
 </script>
 <template>
   <div class="px-4 pt-16">
+    <div
+      class="fixed top-0 left-0 z-10 w-full h-[4rem] bg-white dark:bg-black bg-header-blog"
+    ></div>
     <h2>{{ blogPost.title }}</h2>
     <ContentDoc />
   </div>
