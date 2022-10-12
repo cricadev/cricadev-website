203e757f2e1c851afc9ffac75782661e292bfb42
CricaDev <cristiancasallas998@gmail.com>
2022-10-12 09:09:31 -0500

    usehead in 404

--- pages/404.vue
+++ pages/404.vue
@@ -41,6 +41,20 @@ const props = defineProps({
 });
 
 const handleError = () => clearError({ redirect: "/" });
+useHead({
+  title: "CricaDev | 404",
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      hid: "description",
+      name: "description",
+      content:
+        "Hey, this is a 404, it's not you it's me... Have some fun here!",
+    },
+  ],
+  link: [{ rel: "icon", type: "image/png", href: "/404-img.png" }],
+});
 </script>
 <style lang="scss">
 .rotate-face {
