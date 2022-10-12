f121e4345fac980d1204a170cf38c71d905d394a
CricaDev <cristiancasallas998@gmail.com>
2022-10-12 09:05:59 -0500

    added grid to the error

--- pages/error.vue
+++ pages/error.vue
@@ -1,5 +1,7 @@
 <template lang="">
-  <div></div>
+  <div
+    class="grid w-full h-screen py-32 text-center place-items-center lg:py-24 xs:py-8"
+  ></div>
 </template>
 <script></script>
 <style lang=""></style>
