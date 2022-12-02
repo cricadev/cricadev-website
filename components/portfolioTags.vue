c03f9606fc5b376b9b4eba24821b89ef8aa61ba2
CricaDev <cristiancasallas998@gmail.com>
2022-12-01 21:06:42 -0500

    icons for projects tags implemented

--- components/portfolioTags.vue
+++ components/portfolioTags.vue
@@ -33,42 +33,12 @@ const updateCheckall = (lang) => {
   }
 };
 const getNameTag = (lang) => {
-  if (lang == "Javascript") {
-    return "akar-icons:javascript-fill";
-  } else if (lang == "Vue") {
-    return "akar-icons:vue-fill";
-  } else if (lang == "CSS") {
-    return "akar-icons:css-fill";
-  } else if (lang == "React") {
-    return "akar-icons:react-fill";
-  } else if (lang == "NodeJS") {
-    return "akar-icons:node-fill";
-  } else if (lang == "Typescript") {
-    return "akar-icons:typescript-fill";
-  } else if (lang == "Css") {
-    return "akar-icons:css-fill";
-  } else if (lang == "HTML") {
-    return "akar-icons:html-fill";
-  } else if (lang == "Sass") {
-    return "akar-icons:sass-fill";
-  } else if (lang == "Tailwind") {
-    return "file-icons:tailwind";
-  } else if (lang == "Nuxt") {
-    return "cib:nuxt-js";
-  } else if (lang == "Github") {
-    return "akar-icons:github-fill";
-  } else if (lang == "Git") {
-    return "bi:git";
-  } else if (lang == "NPM") {
-    return "akar-icons:npm-fill";
-  } else if (lang == "GraphQL") {
-    return "akar-icons:graphql-fill";
-  } else if (lang == "Coding") {
-    return "ant-design:code-filled";
-  } else if (lang == "Bootstrap") {
-    return "akar-icons:bootstrap-fill";
-  } else if (lang == "About me") {
-    return "../public/icon.png";
+  if (lang == "Apps") {
+    return "material-symbols:app-registration";
+  } else if (lang == "Landing Pages") {
+    return "fluent-mdl2:reopen-pages";
+  } else if (lang == "Portfolios") {
+    return "ph:paint-brush-bold";
   }
 };
 const showMore = ref(true);
