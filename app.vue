7b7957d3eb352f663e57a82b227e91713559c5a9
CricaDev <cristiancasallas998@gmail.com>
2022-12-20 15:14:59 -0500

    implemented hotjar trackng

--- app.vue
+++ app.vue
@@ -8,7 +8,25 @@
     </NuxtLayout>
   </div>
 </template>
-<script setup></script>
+<script setup>
+useHead({
+  script: [
+    {
+      hid: "hotjar-tracking-script",
+      innerHTML: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3296207,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+      type: "text/javascript",
+      charset: "utf-8",
+    },
+  ],
+});
+</script>
 <style>
 .page-enter-active,
 .page-leave-active {
