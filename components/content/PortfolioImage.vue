10cfa8bf4b6d161d614debbd7fd95b196ca7cf72
CristianN998 <cristiancasallas998@gmail.com>
2023-10-16 20:52:04 -0500

    links dark mode

--- components/content/PortfolioImage.vue
+++ components/content/PortfolioImage.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="">
     <div class="flex justify-center gap-8">
-      <a target="_blank" href="https://countgod.cricadev.com" class="text-white underline">Visit CountGod</a>
-      <a target="_blank" href="https://github.com/cricadev/CountGod" class="text-white underline">Take a look at the
+      <a target="_blank" href="https://countgod.cricadev.com" class="text-black underline dark:text-white">Visit
+        CountGod</a>
+      <a target="_blank" href="https://github.com/cricadev/CountGod" class="text-black underline dark:text-white">Take a
+        look at the
         code</a>
     </div>
     <nuxt-img :src="src" provider="cloudinary">
